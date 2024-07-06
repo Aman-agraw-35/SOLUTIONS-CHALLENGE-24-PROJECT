73ee734ca295336ebd725e6ee511bfbe242580cd
Aman Agrawal <119893462+Aman-agraw-35@users.noreply.github.com>
2024-07-06 23:24:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   images: {
-    domains: ['files.prokerala.com', 'th.bing.com', 'saltandlight.sg', 'www.kentonline.co.uk', 'www.livechennai.com','https://curlytales.com'],
+    domains: ['files.prokerala.com', 'th.bing.com', 'saltandlight.sg', 'www.kentonline.co.uk', 'www.livechennai.com','curlytales.com'],
   },
 }
